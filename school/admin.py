62031e4f5d6ff1baf4f3378419bd80043eab4c9e
FmsJoao <joao.victorfmaciel@gmail.com>
2022-11-21 23:15:45 -0300

    Continuing the study on django

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display=('id','name','rg','cpf','birth_date')
+    list_display_links = ('id','name')
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'code_course', 'description')
+    list_display_links = ('id','code_course')
+    search_fields = ('code_course',)
+
+admin.site.register(Course, Courses)
+
+
+
--- school/models.py
+++ school/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Student (models.Model):
+    name = models.CharField (max_length=30)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Course (models.Model):
+    LEVEL = (
+        ('B', 'Basic'),
+        ('I', 'intermediary'),
+        ('A', 'advanced')
+    )
+    code_course = models.CharField(max_length=10)
+    description = models.CharField(max_length=200)
+    level = models.CharField(max_length=1, choices=LEVEL, blank=False, null=False, default='B')
+
+    def __str__(self):
+        return self.description
